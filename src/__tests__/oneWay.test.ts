c5ea95bbef5893269111795457b3a62979ba5136
Michaël Zasso <targos@protonmail.com>
2019-08-02 15:31:32 +0200

    add one-way tests with tooth growth dataset

--- src/__tests__/oneWay.test.ts
+++ src/__tests__/oneWay.test.ts
@@ -1,6 +1,8 @@
 import { getNumbers, getClasses } from 'ml-dataset-iris';
 import { Matrix } from 'ml-matrix';
 
+import ToothGrowth from './ToothGrowth.json';
+
 import { oneWay } from '..';
 
 const irisData = new Matrix(getNumbers());
@@ -36,4 +38,22 @@ test('iris dataset compared with R', () => {
   expect(result.freedom).toStrictEqual([2, 147]);
   expect(result.testValue).toBeCloseTo(119.3, 1);
   expect(result.pValue).toBeLessThan(2e-16);
+});
+
+test('tooth growth dataset compared with R', () => {
+  const len = ToothGrowth.map((t) => t.len);
+  const supp = ToothGrowth.map((t) => t.supp);
+  const dose = ToothGrowth.map((t) => t.dose);
+
+  const resultSupp = oneWay(len, supp);
+  expect(resultSupp.rejected).toBe(false);
+  expect(resultSupp.freedom).toStrictEqual([1, 58]);
+  expect(resultSupp.testValue).toBeCloseTo(3.668, 1);
+  expect(resultSupp.pValue).toBeCloseTo(0.0604, 4);
+
+  const resultDose = oneWay(len, dose);
+  expect(resultDose.rejected).toBe(true);
+  expect(resultDose.freedom).toStrictEqual([2, 57]);
+  expect(resultDose.testValue).toBeCloseTo(67.42, 2);
+  expect(resultDose.pValue).toBeCloseTo(9.53e-16, 16);
 });
