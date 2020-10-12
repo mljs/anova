6a54ccfc094f699afd16e9755405878ffc184fc1
Michaël Zasso <targos@protonmail.com>
2020-10-12 08:28:04 +0200

    chore: fix types after ml-array-mean update

--- src/libs.d.ts
+++ src/libs.d.ts
@@ -1,8 +1,3 @@
-declare module 'ml-array-mean' {
-  function arrayMean(array: number[]): number;
-  export = arrayMean;
-}
-
 declare module 'cephes' {
   export function fdtrc(d1: number, d2: number, fValue: number): number;
 }
